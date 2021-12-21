2f7fbf7544c12593fa1f9ef8a51f68ff7b8e81bd
rboddy <avidryanb@gmail.com>
2021-12-21 06:27:31 -0500

    Initial commit

---
+++ src/lib/components/layout/nav.svelte
@@ -0,0 +1,44 @@
+<script></script>
+
+<nav class="custom-nav">
+    <div class="nav-menu">
+        <a href="/" class="brand">ryanboddy.net</a>
+        <div></div>
+        <a href="/contact">contact</a>
+        <a href="/blog" class="btnLink">blog</a>
+    </div>
+</nav>
+
+<style>
+    .brand {
+        font-size: 20px;
+        font-weight: bold;
+        font-family: 'Asap Condensed', sans-serif;
+    }
+    a {
+        color: var(--nav-color);
+        text-decoration: none;
+        font-size: 18px;
+        text-align: center;
+        font-weight: bold;
+    }
+    .custom-nav {
+        width: 100%;
+        height: 50px;
+        background-color: var(--nav-bg)
+    }
+    .nav-menu {
+        display: grid;
+        height: 50px;
+        grid-template-columns: 1fr 9fr repeat(2, 1fr);
+        align-items: center;
+    }
+    .btnLink {
+        background-color: var(--btn-bg);
+        width: 100px;
+        padding: 5px;
+        border-radius: 90px;
+        color: var(--btn-color);
+        box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Nav from '$lib/components/layout/nav.svelte';
+    import { page } from '$app/stores';
+</script>
+
+{#if $page.path === '/'}
+    <Nav --nav-bg='#0F292F' --nav-color='#00E9FF' --btn-color='#0F292F' --btn-bg='#00E9FF' />
+{:else if $page.path === '/contact'}
+    <Nav --nav-bg='#00E9FF' --nav-color='#0F292F' --btn-color="#00E9FF" --btn-bg='#0F292F' />
+{:else}
+    <Nav --nav-bg='#CB2D6F' --nav-color='#0F292F' --btn-color="#00E9FF" --btn-bg='#0F292F' />
+{/if}
+
+<main>
+    <slot />
+</main>
+
+<style>
+    :global(body) {
+		margin: 0;
+        font-family: 'Open Sans', sans-serif;
+	}
+</style>
