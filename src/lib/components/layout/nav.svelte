830063387baf401e77ba443db45c7b94ebfe8392
rboddy <avidryanb@gmail.com>
2021-12-27 23:20:35 -0500

    Mobile menu is working

--- src/lib/components/layout/nav.svelte
+++ src/lib/components/layout/nav.svelte
@@ -1,4 +1,11 @@
-<script></script>
+<script>
+    import { slide } from 'svelte/transition';
+    let visible = false;
+
+    function toggle() {
+        visible = !visible;
+    }
+</script>
 
 <nav class="custom-nav">
     <div class="nav-menu">
@@ -8,13 +15,32 @@
         <a href="/blog"><i class="fas fa-typewriter"></i> blog</a>
         <a href="/" class="btnLink"><i class="fa-brands fa-discord"></i> Join</a>
     </div>
+    <div class="mobileNavBtn">
+        <div></div>
+        <button on:click={toggle} class="hamburger"><i class="fas fa-bars"></i></button>
+    </div>
 </nav>
+{#if visible}
+<div class="mobileNav" transition:slide >
+    <a class="mobileLink" href="/"><i class="fas fa-home"></i> Home</a>
+    <hr />
+    <a class="mobileLink" href="/contact"><i class="far fa-envelope-square"></i> Contact</a>
+    <hr />
+    <a class="mobileLink" href="/blog"><i class="fas fa-typewriter"></i> Blog</a>
+    <hr />
+    <a class="mobileLink" href="/"><i class="fa-brands fa-discord"></i> Join</a>
+    <hr />
+</div>
+{/if}
 
 <style>
     .brand {
         font-size: 30px;
         font-family: 'Fjalla One', sans-serif;
         font-weight: normal;
+    }
+    .hamburger {
+        display: none;
     }
     a {
         color: #EBE8E6;
@@ -42,12 +68,37 @@
         color: #1D2030;
         box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
     }
+    .mobileNav {
+        display: none;
+        text-align: right;
+    }
+    .mobileLink {
+        padding-right: 10px;
+    }
     @media only screen 
         and (min-device-width: 320px) 
         and (max-device-width: 480px)
         and (-webkit-min-device-pixel-ratio: 2) {
             .nav-menu {
                 display: none;
+            }
+            .mobileNavBtn {
+                height: 80px;
+                display: grid;
+                grid-template-columns: 4fr 1fr;
+                place-items: center;
+            }
+            .hamburger {
+                display: block;
+                font-size: 30px;
+                background: none;
+                border: none;
+                color: white;
+            }
+            .mobileNav {
+                display: block;
+                background-color: #1D2030;
+                border-top: 1px solid white;
             }
     }
 </style>
