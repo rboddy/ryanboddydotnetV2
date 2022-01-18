f29ae300e0217d57259293f9b56461286ebdd89e
rboddy <avidryanb@gmail.com>
2022-01-17 22:22:06 -0500

    Small fix for category buttons

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -72,6 +72,7 @@
         box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
         background-color: #199BED;
         font-size: 16px;
+        color: black;
     }
     .catBtn:hover {
         cursor: pointer;
